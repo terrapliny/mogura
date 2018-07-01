707370133c0242230fe6b5238669128c180d7228
sura-- <terrapliny@gmail.com>
2018-07-02 05:39:54 +0900

    amimation作成, ic_doron_lite.xml作成, デザイン変更

--- app/src/main/java/com/test/mogura/MainActivity.java
+++ app/src/main/java/com/test/mogura/MainActivity.java
@@ -2,6 +2,8 @@ package com.test.mogura;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView saru = findViewById(R.id.mogura_5);
+        saru.startAnimation(AnimationUtils.loadAnimation(this, R.anim.up_down_anim));
+    }
+
+    @Override
+    public void onBackPressed() {
+
     }
 }
---
+++ app/src/main/java/com/test/mogura/MyUtils.java
@@ -0,0 +1,4 @@
+package com.test.mogura;
+
+public class MyUtils {
+}
